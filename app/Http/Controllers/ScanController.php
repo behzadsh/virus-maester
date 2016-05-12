42d2baba7f0e31428ccc11434d0916d3f6e575b1
Behzad Shabani <behzad.shabani@gmail.com>
2016-05-12 12:56:35 +0430

    Handle response and getting report

--- app/Http/Controllers/ScanController.php
+++ app/Http/Controllers/ScanController.php
@@ -4,6 +4,8 @@ namespace Maester\Http\Controllers;
 
 use Maester\Http\Requests;
 use moay\VirusTotalApi\VirusTotalApi;
+use VirusTotal\File;
+use VirusTotal\Url;
 
 class ScanController extends Controller
 {
@@ -14,11 +16,39 @@ class ScanController extends Controller
         $fileHash = hash_file('sha256', $file);
         $file = $file->move(storage_path("files"), "$fileHash.{$file->getClientOriginalExtension()}");
         $response = VirusTotalApi::scanFile($file->getPathname());
-        dd($response);
+
+        if ($report = $this->handleResponse($response, 'file')) {
+            dd($report);
+        }
+
+        return view('errors.api_limits');
     }
 
     public function url()
     {
+    }
+
+    private function handleResponse($response, $type)
+    {
+        if ($response['success']) {
+            /** @var File|Url $virusTotal */
+            $virusTotal = $this->getApiClass($type);
+
+            return $virusTotal->getReport($response['scan_id']);
+        } elseif ($response['error'] == 'rate limit exceeded') {
+            return false;
+        }
+    }
+
+    private function getApiClass($type)
+    {
+        if ($type == 'file') {
+            return new File(env('VT_API_KEY'));
+        } elseif ($type == 'url') {
+            return new Url(env('VT_API_KEY'));
+        }
+
+        throw new \Exception('invalid virus total driver type');
     }
 
 }
