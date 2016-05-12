9cec580c0e341f93e7213fd51385d4f3b55b4d19
Behzad Shabani <behzad.shabani@gmail.com>
2016-05-12 16:31:03 +0430

    Fix UrlScanRequest

--- app/Http/Requests/UrlScanRequest.php
+++ app/Http/Requests/UrlScanRequest.php
@@ -13,7 +13,7 @@ class UrlScanRequest extends Request
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class UrlScanRequest extends Request
     public function rules()
     {
         return [
-            //
+            'url' => 'required'
         ];
     }
 }
